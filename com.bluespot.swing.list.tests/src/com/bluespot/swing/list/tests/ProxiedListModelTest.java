d86bbc91a1fd014c0191ffc012bb4cceb43818cd
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-06-03 09:12:57 +0000

    Removed useless test
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@31 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot.swing.list.tests/src/com/bluespot/swing/list/tests/ProxiedListModelTest.java
+++ com.bluespot.swing.list.tests/src/com/bluespot/swing/list/tests/ProxiedListModelTest.java
@@ -1,17 +1,9 @@
 package com.bluespot.swing.list.tests;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import com.bluespot.swing.list.MutableListModel;
 import com.bluespot.swing.list.ProxiedListModel;
 
 public class ProxiedListModelTest extends MutableListModelTest {
-
-	@Test
-	public void testSomeCustomStuff() {
-		Assert.assertTrue(true);
-	}
 
 	@Override
 	protected MutableListModel<String> newListModel() {
