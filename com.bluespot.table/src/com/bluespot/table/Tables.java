52e4a5d883e9ee2aa71fe9afce05a58399ae6fa3
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-06-03 06:33:05 +0000

    Tables now supports filling via lists
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@17 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot.table/src/com/bluespot/table/Tables.java
+++ com.bluespot.table/src/com/bluespot/table/Tables.java
@@ -1,10 +1,22 @@
 package com.bluespot.table;
 
+import java.util.Arrays;
+import java.util.List;
+
 public final class Tables {
 
 	private Tables() {
 		// Suppresses default constructor, ensuring non-instantiability.
 		throw new AssertionError("This class cannot be instantiated");
+	}
+
+	public static <T> void fill(final Table<T> table, final List<T> values) {
+		int i = 0;
+		for (final TableIterator<T> iter = table.tableIterator(); iter.hasNext();) {
+			iter.next();
+			iter.put(values.get(i++));
+			i %= values.size();
+		}
 	}
 
 	public static <T> void fill(final Table<T> table, final T value) {
@@ -15,12 +27,7 @@ public final class Tables {
 	}
 
 	public static <T> void fill(final Table<T> table, final T... values) {
-		int i = 0;
-		for (final TableIterator<T> iter = table.tableIterator(); iter.hasNext();) {
-			iter.next();
-			iter.put(values[i++]);
-			i %= values.length;
-		}
+		Tables.fill(table, Arrays.asList(values));
 	}
 
 	public static void fillCount(final Table<Integer> table, final int start) {
