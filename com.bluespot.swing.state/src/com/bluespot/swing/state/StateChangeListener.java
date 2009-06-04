5d9a8ae6764f222c4eac1d9dfdfd083f8e45e7ba
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-06-04 06:28:32 +0000

    Removed alot of obsolete projects
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@56 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot.swing.state/src/com/bluespot/swing/state/StateChangeListener.java
+++
@@ -1,7 +0,0 @@
-package com.bluespot.swing.state;
-
-import javax.swing.event.ChangeListener;
-
-public interface StateChangeListener<T> extends ChangeListener {
-	void stateChanging(StateChangeEvent<T> e);
-}
--- com.bluespot.tree/src/com/bluespot/tree/Visitor.java
+++
@@ -1,5 +0,0 @@
-package com.bluespot.tree;
-
-public interface Visitor<T> {
-	public void visitNode(Tree<T> group);
-}
