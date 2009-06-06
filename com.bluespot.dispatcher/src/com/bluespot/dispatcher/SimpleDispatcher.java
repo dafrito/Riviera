3cd498432951cb4d70d29757a3324e51fed83303
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-06-06 21:28:24 +0000

    Removed dispatcher This class hasn't really yielded the usability I hoped for it; it generally causes a proliferation of classes with little to no added clarity
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@64 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot.dispatcher/src/com/bluespot/dispatcher/SimpleDispatcher.java
+++
@@ -1,9 +0,0 @@
-package com.bluespot.dispatcher;
-
-public abstract class SimpleDispatcher<E, L> extends AbstractDispatcher<E, L> implements Dispatchable<E, L> {
-
-	public void dispatch(final E value) {
-		this.dispatch(this, value);
-	}
-
-}
