09a94c4cde57a5df922d6e35860588a701dcbd7b
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-06-06 21:22:00 +0000

    Created an ObservableDeque:  - Removed ObservableQueue and associated classes, since this one serves that purpose better
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@61 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

---
+++ com.bluespot.collections.observable/src/com/bluespot/collections/observable/deque/DequeListener.java
@@ -0,0 +1,60 @@
+package com.bluespot.collections.observable.deque;
+
+import java.util.Deque;
+
+/**
+ * @author Aaron Faanes
+ * @param <E>
+ *            the type of element contained in the observed deque
+ */
+public interface DequeListener<E> {
+
+	/**
+	 * Invoked when a new element has been added at the end of this deque.
+	 * <p>
+	 * The new first element is attainable via {@link Deque#peekFirst()}.
+	 * 
+	 * @param oldFirstElement
+	 *            the previous first element of this deque
+	 */
+	public void firstElementAdded(E oldFirstElement);
+
+	/**
+	 * Invoked when a new element has been added at the end of this deque.
+	 * <p>
+	 * The new last element is attainable via {@link Deque#peekLast()}.
+	 * 
+	 * @param oldLastElement
+	 *            the previous last element of this deque
+	 */
+
+	public void lastElementAdded(E oldLastElement);
+
+	/**
+	 * Invoked when the last element is removed from the observed deque.
+	 * <p>
+	 * The new last element is attainable via {@link Deque#peekLast()}.
+	 * 
+	 * @param oldLastElement
+	 *            the removed element
+	 */
+	public void lastElementRemoved(E oldLastElement);
+
+	/**
+	 * Invoked when the first element is removed from the watched deque.
+	 * <p>
+	 * The new first element is attainable via {@link Deque#peekFirst()}.
+	 * 
+	 * @param oldFirstElement
+	 *            the removed element
+	 */
+	public void firstElementRemoved(E oldFirstElement);
+
+	/**
+	 * Invoked when the deque has changed so dramatically that it cannot be
+	 * described with any other method. This is typically because of non-deque
+	 * like behavior, such as removing elements in the middle of the list and so
+	 * forth.
+	 */
+	public void dequeChanged();
+}
