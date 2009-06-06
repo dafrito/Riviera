f8db035017403846494df9fada4be8d0871c647b
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-06-06 22:16:49 +0000

    Added javadoc to our statemodel stuff
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@83 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot.collections.observable/src/com/bluespot/collections/observable/state/StateChangeAdapter.java
+++ com.bluespot.collections.observable/src/com/bluespot/collections/observable/state/StateChangeAdapter.java
@@ -2,6 +2,14 @@ package com.bluespot.collections.observable.state;
 
 import javax.swing.event.ChangeEvent;
 
+/**
+ * A no-op {@link StateChangeListener} implementation.
+ * 
+ * @author Aaron Faanes
+ * 
+ * @param <T>
+ *            the type of state in the observed model
+ */
 public class StateChangeAdapter<T> implements StateChangeListener<T> {
 
 	public void stateChanged(final ChangeEvent e) {
