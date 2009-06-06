934a11651538dd2ead7272019921d4d68aa5f399
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-06-06 22:17:03 +0000

    added javadoc to dequeAdapter
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@84 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot.collections.observable/src/com/bluespot/collections/observable/deque/DequeAdapter.java
+++ com.bluespot.collections.observable/src/com/bluespot/collections/observable/deque/DequeAdapter.java
@@ -1,5 +1,13 @@
 package com.bluespot.collections.observable.deque;
 
+/**
+ * A no-op {@link DequeListener} implementation.
+ * 
+ * @author Aaron Faanes
+ * 
+ * @param <E>
+ *            the type of element in the observed deque
+ */
 public class DequeAdapter<E> implements DequeListener<E> {
 
 	public void dequeChanged() {
