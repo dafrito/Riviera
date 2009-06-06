8630156101e8ca4e2cf402e7ab19b48914b55c0e
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-06-06 22:38:32 +0000

    Cleaned up AllRivieraTests
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@85 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot-tests/src/com/bluespot/tests/AllRivieraTests.java
+++ com.bluespot-tests/src/com/bluespot/tests/AllRivieraTests.java
@@ -3,6 +3,7 @@ package com.bluespot.tests;
 import java.io.File;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -27,90 +28,146 @@ import junit.framework.TestSuite;
  * @author Aaron Faanes
  * 
  */
-public class AllRivieraTests {
+public final class AllRivieraTests {
 
 	/**
 	 * Tests to explicitly exclude, for whatever reason
 	 */
-	private static final Set<String> EXCLUDED = new HashSet<String>(Arrays.asList("com.bluespot.junit.InjectTest"));
+	private static final Set<String> EXCLUDED = new HashSet<String>(Arrays.asList("com.bluespot.junit.InjectTest",
+			AllRivieraTests.class.getName()));
 
 	/**
 	 * A set of tests, populated when this is run as a JUnit test.
 	 */
-	private static final Set<Class<?>> TESTS = new HashSet<Class<?>>();
+	private final Set<Class<?>> tests = new HashSet<Class<?>>();
 
-	private static String getChildPackage(final String parentPackageName, final String childPackage) {
-		if (parentPackageName.equals("")) {
-			return childPackage;
-		}
-		return parentPackageName + "." + childPackage;
+	private final StringBuilder currentPackage = new StringBuilder();
+
+	private String getCurrentPackage() {
+		return this.currentPackage.toString();
 	}
 
-	private static String getClassNameFromFile(final File file) {
+	private void enterNestedPackage(final String nestedPackageName) {
+		if (this.currentPackage.length() == 0) {
+			this.currentPackage.append(nestedPackageName);
+		} else {
+			this.currentPackage.append('.');
+			this.currentPackage.append(nestedPackageName);
+		}
+	}
+
+	private void leaveNestedPackage(final String name) {
+		if (this.currentPackage.length() == name.length()) {
+			this.currentPackage.setLength(0);
+		} else {
+			// We subtract an additional one for the "." in between package
+			// names
+			this.currentPackage.substring(0, this.currentPackage.length() - name.length() - 1);
+		}
+	}
+
+	private String getClassNameFromFile(final File file) {
 		final String fileName = file.getName();
 		return fileName.substring(0, fileName.lastIndexOf('.'));
 	}
 
-	public static void addClassesFrom(final String parentPackageName, final File file) {
+	/**
+	 * Adds all test classes from the specified file. If the file is a folder,
+	 * then it will be recursed.
+	 * 
+	 * @param file
+	 *            the file that potentially is, or potentially contains, test
+	 *            classes
+	 */
+	public void addClassesFrom(final File file) {
 		if (file.getName().startsWith(".")) {
 			// Return; it's not interesting to us.
 			return;
 		}
-		if (AllRivieraTests.isTestClass(file)) {
-			final String className = parentPackageName + "." + getClassNameFromFile(file);
+		if (this.isTestClass(file)) {
+			final String className = this.getCurrentPackage() + "." + this.getClassNameFromFile(file);
 			if (EXCLUDED.contains(className)) {
 				return;
 			}
 			try {
-				TESTS.add(Class.forName(className));
+				this.tests.add(Class.forName(className));
 			} catch (final ClassNotFoundException e) {
 				System.out.printf("Class looks like a test, but couldn't be found: %s%n", className);
 			}
 			return;
 		}
-		final String packageName = getChildPackage(parentPackageName, file.getName());
+		this.enterNestedPackage(file.getName());
 		final File[] children = file.listFiles();
 		if (children == null) {
 			return;
 		}
 		for (final File child : children) {
-			AllRivieraTests.addClassesFrom(packageName, child);
+			this.addClassesFrom(child);
 		}
+		this.leaveNestedPackage(file.getName());
 	}
 
-	private static boolean isTestClass(final File file) {
+	private boolean isTestClass(final File file) {
 		return file.getName().endsWith("Test.java") || file.getName().endsWith("Tests.java");
 	}
 
-	private static boolean isProjectDir(final File file) {
+	private boolean isProjectDir(final File file) {
 		return file.isDirectory() && !file.getName().startsWith(".");
 	}
 
-	public static void addProject(final File projectFile) {
-		if (!AllRivieraTests.isProjectDir(projectFile)) {
+	/**
+	 * Adds all test files in the specified project to this object's tests set.
+	 * 
+	 * @param projectFile
+	 *            the folder containing the test files. The folder should be
+	 *            structured like an Eclipse project
+	 */
+	public void addProject(final File projectFile) {
+		if (!this.isProjectDir(projectFile)) {
 			return;
 		}
 		final File srcDir = new File(projectFile, "src");
 		if (srcDir.exists() && srcDir.isDirectory()) {
 			for (final File child : srcDir.listFiles()) {
-				AllRivieraTests.addClassesFrom("", child);
+				this.addClassesFrom(child);
 			}
 		}
 	}
 
+	/**
+	 * Returns the list of tests this object has found.
+	 * 
+	 * @return the list of tests this object has found
+	 * @see #addProject(File)
+	 * @see #addClassesFrom(File)
+	 */
+	public Set<Class<?>> getTests() {
+		return Collections.unmodifiableSet(this.tests);
+	}
+
+	/**
+	 * Creates a new test suite out of all the test projects in Riviera.
+	 * <p>
+	 * Any test that is intended to be found here must be on the classpath for
+	 * this project. Refer to {@link AllRivieraTests} for more information
+	 * concerning which tests will be included.
+	 * 
+	 * @return a test suite containing all test classes in Riviera
+	 */
 	public static Test suite() {
 		final TestSuite suite = new TestSuite("Riviera Tests");
+
+		final AllRivieraTests tests = new AllRivieraTests();
+
 		for (final File child : new File("..").listFiles()) {
-			AllRivieraTests.addProject(child);
+			tests.addProject(child);
 		}
-		// Explicitly exclude this class; it seems like there's be serious
-		// recursion issues if we included it.
-		AllRivieraTests.TESTS.remove(AllRivieraTests.class);
-		for (final Class<?> klass : AllRivieraTests.TESTS) {
+		for (final Class<?> klass : tests.getTests()) {
 			if (!Modifier.isAbstract(klass.getModifiers()) && klass.getConstructors().length > 0) {
 				suite.addTest(new JUnit4TestAdapter(klass));
 			}
 		}
+
 		return suite;
 	}
 }
