d1c436314ca00c7ab1e6b7929c6e623f870ee26c
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-06-10 18:50:32 +0000

    Cleaned up BeanView, added javadoc
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@107 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot.swing/src/com/bluespot/swing/BeanForm.java
+++ com.bluespot.swing/src/com/bluespot/swing/BeanForm.java
@@ -1,6 +1,5 @@
 package com.bluespot.swing;
 
-import java.awt.Graphics;
 import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
@@ -13,13 +12,32 @@ import java.util.Map;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class BeanView<T> extends JPanel {
+/**
+ * A very simple and incomplete view of a bean.
+ * 
+ * @author Aaron Faanes
+ * 
+ * @param <T>
+ *            the type of the viewed bean
+ */
+public class BeanForm<T> extends JPanel {
+
+	private static final long serialVersionUID = -8964151905286569804L;
 
 	private BeanInfo beanInfo;
 	private final Map<String, JTextField> fieldMap = new HashMap<String, JTextField>();
 	private T value;
 
-	public BeanView(final Class<T> viewedClass) throws IntrospectionException {
+	/**
+	 * Constructs a bean view that views the specified class.
+	 * 
+	 * @param viewedClass
+	 *            the viewed class
+	 * @throws IntrospectionException
+	 *             if bean introspection fails
+	 * @see Introspector#getBeanInfo(Class)
+	 */
+	public BeanForm(final Class<T> viewedClass) throws IntrospectionException {
 
 		final GroupLayoutBuilder builder = new GroupLayoutBuilder(this);
 
@@ -39,28 +57,43 @@ public class BeanView<T> extends JPanel {
 		builder.build();
 	}
 
+	/**
+	 * Constructs a bean view that views the specified value.
+	 * 
+	 * @param value
+	 *            the viewed value
+	 * @throws IntrospectionException
+	 *             if bean introspection fails
+	 * @see Introspector#getBeanInfo(Class)
+	 */
 	@SuppressWarnings("unchecked")
-	public BeanView(final T value) throws IntrospectionException {
+	public BeanForm(final T value) throws IntrospectionException {
+		// Guaranteed to succeed
 		this((Class<T>) value.getClass());
 		this.setValue(value);
 	}
 
+	/**
+	 * Returns the current viewed value.
+	 * 
+	 * @return the current viewed value
+	 */
 	public T getValue() {
 		return this.value;
 	}
 
+	/**
+	 * Sets the viewed value
+	 * 
+	 * @param value
+	 *            the new viewed value
+	 */
 	public void setValue(final T value) {
 		this.value = value;
 		this.refresh();
 	}
 
-	@Override
-	protected void printComponent(final Graphics g) {
-		super.printComponent(g);
-
-	}
-
-	protected void refresh() {
+	private void refresh() {
 		if (this.value == null) {
 			this.setEnabled(false);
 			return;
