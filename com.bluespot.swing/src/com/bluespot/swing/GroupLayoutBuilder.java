d90c6dba8b374577e3934db0f780441a87b6eef2
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-06-10 18:52:04 +0000

    Added javadoc to GroupLayoutBuilder, removed a couple useless getters
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@110 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot.swing/src/com/bluespot/swing/GroupLayoutBuilder.java
+++ com.bluespot.swing/src/com/bluespot/swing/GroupLayoutBuilder.java
@@ -1,21 +1,40 @@
 package com.bluespot.swing;
 
+import javax.swing.BoxLayout;
 import javax.swing.GroupLayout;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.GroupLayout.Group;
 
+/**
+ * Assists in building a layout with {@link GroupLayout}. The idiom here is to
+ * build iteratively.
+ * 
+ * @author Aaron Faanes
+ * 
+ */
 public class GroupLayoutBuilder {
 
 	private final JComponent component;
 	private final Group fieldGroup;
-	private Group horizontalGroup;
+	private final Group horizontalGroup;
 	private final Group labelGroup;
 	private final GroupLayout layout;
-	private Group verticalGroup;
+	private final Group verticalGroup;
 
+	/**
+	 * Constructs a builder for the specified component.
+	 * 
+	 * @param component
+	 *            the component that will be targeted with this builder
+	 * @throws NullPointerException
+	 *             if {@code component} is null
+	 */
 	public GroupLayoutBuilder(final JComponent component) {
+		if (component == null) {
+			throw new NullPointerException("component is null");
+		}
 		this.component = component;
 		this.layout = new GroupLayout(this.component);
 		this.horizontalGroup = this.layout.createParallelGroup();
@@ -27,25 +46,61 @@ public class GroupLayoutBuilder {
 				this.fieldGroup));
 	}
 
-	public void add(final JComponent comp) {
+	/**
+	 * Adds the specified component that is laid out directly beneath the
+	 * previous component. Repeated calls to this method would yield behavior
+	 * similar to {@link BoxLayout}.
+	 * 
+	 * @param comp
+	 *            the added component
+	 * @return this builder object
+	 */
+	public GroupLayoutBuilder add(final JComponent comp) {
 		this.horizontalGroup.addComponent(comp);
 		this.verticalGroup.addComponent(comp);
+		return this;
 	}
 
-	public void add(final String string) {
+	/**
+	 * Adds a {@link JLabel} that contains the specified text to this builder.
+	 * It will be laid out directly beneath any previous components.
+	 * 
+	 * @param string
+	 *            the label text
+	 * @return this builder object
+	 * @see GroupLayoutBuilder#add(JComponent)
+	 */
+	public GroupLayoutBuilder add(final String string) {
 		this.add(new JLabel(string));
+		return this;
 	}
 
-	public void addLabeledPair(final String labelName, final JComponent comp) {
+	/**
+	 * Adds a form field to this builder. It will be directly beneath any
+	 * previous components.
+	 * 
+	 * @param labelName
+	 *            the label text
+	 * @param comp
+	 *            the added component
+	 * @return this builder object
+	 */
+	public GroupLayoutBuilder addLabeledPair(final String labelName, final JComponent comp) {
 		final JLabel label = new JLabel(labelName);
 		this.labelGroup.addComponent(label);
 		this.fieldGroup.addComponent(comp);
 		this.verticalGroup.addGroup(this.layout.createParallelGroup(Alignment.BASELINE)
 				.addComponent(label)
 				.addComponent(comp));
+		return this;
 	}
 
-	public void build() {
+	/**
+	 * Finishes any work on this builder and returns the laid-out component.
+	 * 
+	 * @return the constructed component
+	 */
+	public JComponent build() {
 		this.layout.setAutoCreateContainerGaps(true);
 		this.layout.setAutoCreateGaps(true);
 
@@ -53,22 +108,7 @@ public class GroupLayoutBuilder {
 		this.layout.setVerticalGroup(this.verticalGroup);
 
 		this.component.setLayout(this.layout);
-	}
-
-	public Group getHorizontalGroup() {
-		return this.horizontalGroup;
-	}
-
-	public Group getVerticalGroup() {
-		return this.verticalGroup;
-	}
-
-	public void setHorizontalGroup(final Group horizontalGroup) {
-		this.horizontalGroup = horizontalGroup;
-	}
-
-	public void setVerticalGroup(final Group verticalGroup) {
-		this.verticalGroup = verticalGroup;
+		return this.component;
 	}
 
 }
