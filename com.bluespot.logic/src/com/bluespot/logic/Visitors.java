63efd493575c8ec95c777acc65cf504865677e02
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-06-14 11:20:57 +0000

    Visitors: Added "when" visitor, and renamed other visitors for readability purposes. For example:
    when(value().is(notNullValue()), addTo(strings);
    
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@154 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot.logic/src/com/bluespot/logic/Visitors.java
+++ com.bluespot.logic/src/com/bluespot/logic/Visitors.java
@@ -5,12 +5,26 @@ import java.util.Collection;
 import junit.framework.AssertionFailedError;
 
 import com.bluespot.logic.adapters.Adapter;
+import com.bluespot.logic.predicates.Predicate;
 import com.bluespot.logic.visitors.AdaptingVisitor;
 import com.bluespot.logic.visitors.PopulatingVisitor;
 import com.bluespot.logic.visitors.PruningVisitor;
+import com.bluespot.logic.visitors.SingleSentinel;
+import com.bluespot.logic.visitors.Visitor;
 
 /**
- * A collection of factory methods for common sentinels and visitors.
+ * A collection of factory methods for common sentinels and visitors. The names
+ * of these methods are intentionally "conversational" since this library is
+ * usually statically imported. If this is the case, you can end up with such
+ * statements like:
+ * 
+ * <pre>
+ * when(lowerCase(), addToList(strings));
+ * </pre>
+ * 
+ * On the other hand, sporadic uses are much harder to understand. In those
+ * cases, you're better off using the visitor constructors directly.
+ * 
  * 
  * @author Aaron Faanes
  * 
@@ -20,6 +34,27 @@ public final class Visitors {
     private Visitors() {
         // Suppress default constructor to ensure non-instantiability.
         throw new AssertionFailedError("Instantiation not allowed");
+    }
+
+    /**
+     * Returns a new {@link SingleSentinel} that guards the specified visitor
+     * with the specified predicate.
+     * 
+     * @param <T>
+     *            the type of the expected element
+     * @param predicate
+     *            the predicate that guards the specified visitor. For any
+     *            value, the predicate must first evaluate to {@code true}
+     *            before the specified visitor is allowed to accept it.
+     * @param visitor
+     *            the visitor that will accept all items that evaluate to
+     *            {@code true} according to the specified predicate
+     * @return a new {@code SingleSentinel} that guards the specified visitor
+     *         with the specified predicate.
+     * @see SingleSentinel
+     */
+    public static <T> SingleSentinel<T> when(final Predicate<? super T> predicate, final Visitor<? super T> visitor) {
+        return new SingleSentinel<T>(predicate, visitor);
     }
 
     /**
@@ -41,7 +76,7 @@ public final class Visitors {
      * @throws NullPointerException
      *             if either argument is null
      */
-    public static <S, D> Visitor<S> withAdapter(final Adapter<? super S, ? extends D> adapter,
+    public static <S, D> Visitor<S> with(final Adapter<? super S, ? extends D> adapter,
             final Visitor<? super D> targetVisitor) {
         return new AdaptingVisitor<S, D>(adapter, targetVisitor);
     }
@@ -58,7 +93,7 @@ public final class Visitors {
      * @return a new {@code Visitor} that adds elements to the specified
      *         collection
      */
-    public static <T> Visitor<T> populate(final Collection<? super T> collection) {
+    public static <T> Visitor<T> addTo(final Collection<? super T> collection) {
         return new PopulatingVisitor<T>(collection);
     }
 
@@ -74,7 +109,7 @@ public final class Visitors {
      * @return a new {@code Visitor} that adds elements to the specified
      *         collection
      */
-    public static <T> Visitor<T> depopulate(final Collection<? super T> collection) {
+    public static <T> Visitor<T> removeFrom(final Collection<? super T> collection) {
         return new PruningVisitor<T>(collection);
     }
 }
