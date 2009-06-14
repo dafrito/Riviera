4a7cee60446c179a68b1e4a81b1ea61215f510c1
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-06-14 09:34:04 +0000

    Removed the ability to subclass RegexPredicate. Use an adapter combined with a predicate if you want to change the getString behavior of this class.
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@141 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot.logic/src/com/bluespot/logic/predicates/RegexPredicate.java
+++ com.bluespot.logic/src/com/bluespot/logic/predicates/RegexPredicate.java
@@ -6,17 +6,13 @@ import java.util.regex.Pattern;
  * Creates a predicate that evaluates the string version of some value. It will
  * return {@code true} if and only if the specified pattern matches its string
  * value.
- * <p>
- * The string value is determined by {@link #getString(Object)}. Subclasses may
- * override this method if they wish to specify a different method of obtaining
- * a string value.
  * 
  * @author Aaron Faanes
  * 
  * @param <T>
  *            the value tested in this predicate
  */
-public class RegexPredicate<T> implements Predicate<T> {
+public final class RegexPredicate<T> implements Predicate<T> {
 
     private final Pattern pattern;
 
@@ -42,25 +38,12 @@ public class RegexPredicate<T> implements Predicate<T> {
         return this.pattern;
     }
 
-    /**
-     * Returns the string version of the specified non-null value.
-     * <p>
-     * By default, this calls {@link Object#toString()}.
-     * 
-     * @param value
-     *            the value used to generate the string
-     * @return the string value of the specified value
-     */
-    protected String getString(final T value) {
-        return value.toString();
-    }
-
     @Override
     public boolean test(final T value) {
         if (value == null) {
             return false;
         }
-        return this.getPattern().matcher(this.getString(value)).matches();
+        return this.getPattern().matcher(value.toString()).matches();
     }
 
 }
