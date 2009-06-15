d485a8586e0711959e802112bfd0f7ef0efea93e
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-06-15 08:28:53 +0000

    Added some detail to Adapter's contract
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@157 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot.logic/src/com/bluespot/logic/adapters/Adapter.java
+++ com.bluespot.logic/src/com/bluespot/logic/adapters/Adapter.java
@@ -1,8 +1,13 @@
 package com.bluespot.logic.adapters;
 
+import com.bluespot.logic.Adapters;
+import com.bluespot.logic.predicates.AdaptingPredicate;
+import com.bluespot.logic.predicates.Predicate;
+
 /**
- * Adapts a value to another type. Null values should either be converted or
- * preserved.
+ * Adapts a given value to another type. Many common adapters can be obtained
+ * from the static factory methods in {@link Adapters}. Adapters are also widely
+ * used by {@link Predicate} and their builders.
  * 
  * @author Aaron Faanes
  * 
@@ -10,14 +15,19 @@ package com.bluespot.logic.adapters;
  *            the source type
  * @param <D>
  *            the destination type
+ * @see Adapters
+ * @see AdaptingPredicate
  */
 public interface Adapter<S, D> {
 
     /**
-     * Adapts the specified value to this adapter's destination type.
-     * <p>
-     * Null values should be reasonably converted or preserved during the
-     * adaption; do not assume that only non-null values will be used.
+     * Adapts the specified value to this adapter's destination type. Adapters
+     * should make a best effort to return appropriate values for the widest
+     * range of inputs; exceptions should rarely be thrown. Forgiving adapters
+     * allow them to be used freely without unexpected results cropping up.
+     * However, null values are commonly preserved in adapters; a null source
+     * value should be converted to a null value unless the adapter explicitly
+     * states otherwise.
      * 
      * @param source
      *            the source value to adapt
