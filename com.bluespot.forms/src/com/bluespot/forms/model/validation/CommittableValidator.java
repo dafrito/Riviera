08f3d85d6b496238388dc74b6e8486f3edbe1c1d
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-06-19 16:19:08 +0000

    [addresses #2] CommittableValidator is now a final class, made a protected instance now private
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@201 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot.forms/src/com/bluespot/forms/model/validation/CommittableValidator.java
+++ com.bluespot.forms/src/com/bluespot/forms/model/validation/CommittableValidator.java
@@ -2,13 +2,13 @@ package com.bluespot.forms.model.validation;
 
 import com.bluespot.forms.model.commit.Committable;
 
-public class CommittableValidator extends ListValidator<Committable<?>> {
+public final class CommittableValidator extends ListValidator<Committable<?>> {
 
     public CommittableValidator() {
         super(CommittableValidator.VALIDATOR);
     }
 
-    protected static final Validator<Committable<?>> VALIDATOR = new Validator<Committable<?>>() {
+    private static final Validator<Committable<?>> VALIDATOR = new Validator<Committable<?>>() {
         public ValidationResult<Committable<?>> validate(final Committable<?> committable) {
             return ValidationResult.forwardResult(this, null, committable.commit());
         }
