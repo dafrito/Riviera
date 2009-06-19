bb8f786c89a9ca3b75e70d67d5fbd4f3f8ac1578
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-06-19 13:27:27 +0000

    [javadoc] Added alot of detail to Predicate
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@184 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot.logic/src/com/bluespot/logic/predicates/Predicate.java
+++ com.bluespot.logic/src/com/bluespot/logic/predicates/Predicate.java
@@ -1,11 +1,27 @@
 package com.bluespot.logic.predicates;
 
+import com.bluespot.logic.Predicates;
+import com.bluespot.logic.predicates.builder.PredicateBuilder;
+
 /**
  * An interface that defines a predicate. Predicates represent only boolean
  * methods and should not affect any values in any way. Predicates should accept
  * as wide a range of values as possible; only in the most severe cases should
  * an exception be thrown instead of returning {@code false}. Null values should
  * generally evaluate to {@code false}.
+ * <p>
+ * Predicates can be constructed using a {@link PredicateBuilder}. This allows
+ * you to construct predicates in a readable, domain-specific way. Predicate
+ * builders also provide more imperative, traditional methods if you're not
+ * interesting in working with the DSL.
+ * <p>
+ * Many common predicates have already been written and factory methods for them
+ * reside in {@link Predicates}. Using these in conjunction with builders makes
+ * creating simple predicates easy and complex predicates possible.
+ * <p>
+ * All predicates should implement {@link Object#equals(Object)} appropriately,
+ * even composite ones. This is possible because constructed predicates are
+ * immutable.
  * 
  * @author Aaron Faanes
  * 
@@ -22,7 +38,8 @@ public interface Predicate<T> {
      * Unless otherwise noted, values that are equal should cause this predicate
      * to evaluate in an equal manner. This method should also evaluate
      * consistently given consistent inputs. This intentionally resembles the
-     * requirements for a well-written {@link #equals(Object)} implementation.
+     * requirements for a well-written {@link Object#equals(Object)}
+     * implementation.
      * 
      * @param value
      *            the value to evaluate
