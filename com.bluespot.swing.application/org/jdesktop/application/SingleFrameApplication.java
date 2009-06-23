9f2ab466be452186bea861509d753cafd691f77f
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-06-23 00:30:32 +0000

    SingleFrameApplication has a deprecated no-arg constructor for all our legacy examples
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@234 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot.swing.application/org/jdesktop/application/SingleFrameApplication.java
+++ com.bluespot.swing.application/org/jdesktop/application/SingleFrameApplication.java
@@ -97,6 +97,11 @@ import javax.swing.WindowConstants;
 public abstract class SingleFrameApplication extends Application {
     private static final Logger logger = Logger.getLogger(SingleFrameApplication.class.getName());
 
+    @Deprecated
+    public SingleFrameApplication() {
+        this(new ApplicationContext());
+    }
+
     public SingleFrameApplication(final ApplicationContext context) {
         super(context);
     }
