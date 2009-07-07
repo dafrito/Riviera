c994341b51a439390b5c6d78a70ae788f8f6136d
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-07-07 09:11:45 +0000

    EndsWithPredicate and StartsWithPredicate no longer provide implicit adapters. I liked the idea at the time since it seemed so common that we'd want to adapt objects to strings and use this predicate, but we're just as likely to need adapting for a large number of predicates. Also, the default adapter, stringValue, implies that #toString() is of a contractually-binding format. Since we're opposed to that idea, it makes no sense to have these predicates depend on #toString() for their evaluation. As a consequence, they are now both Predicate<String> objects.
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@331 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot.logic/src/com/bluespot/logic/predicates/EndsWithPredicate.java
+++ com.bluespot.logic/src/com/bluespot/logic/predicates/EndsWithPredicate.java
@@ -1,32 +1,20 @@
 package com.bluespot.logic.predicates;
 
-import com.bluespot.logic.Adapters;
-import com.bluespot.logic.adapters.Adapter;
-
 /**
- * Creates a predicate that evaluates the string version of some value. It will
- * return {@code true} if and only if the tested string ends with the
- * predicate's specified ending string.
- * <p>
- * Values will be converted to strings using the specified adapter. By default,
- * {@link Adapters#stringValue()} is used.
+ * Creates a predicate that evaluates {@link String} values. It will return
+ * {@code true} for a given string if, and only if, the string ends with the
+ * predicate's specified starting string.
  * 
  * @author Aaron Faanes
  * 
- * @param <T>
- *            the value tested in this predicate
  * @see StartsWithPredicate
  */
-public final class EndsWithPredicate<T> implements Predicate<T> {
+public final class EndsWithPredicate implements Predicate<String> {
 
     private final String endingString;
-    private final Adapter<? super T, String> adapter;
 
     /**
-     * Constructs a predicate using the specified string. The predicate will
-     * evaluate to {@code true} if and only if the tested string ends with the
-     * specified string value. Values are converted to string using
-     * {@link Adapters#stringValue()}.
+     * Constructs a {@link EndsWithPredicate} using the specified string.
      * 
      * @param endingString
      *            the value that tested strings must end with
@@ -34,31 +22,10 @@ public final class EndsWithPredicate<T> implements Predicate<T> {
      *             if {@code endingString} is null
      */
     public EndsWithPredicate(final String endingString) {
-        this(endingString, Adapters.stringValue());
-    }
-
-    /**
-     * Constructs a predicate using the specified string. Tested values are
-     * converted to strings using the specified adapter. The predicate will
-     * evaluate to {@code true} if and only if the tested string ends with the
-     * specified string value.
-     * 
-     * @param endingString
-     *            the value that tested strings must end with
-     * @param adapter
-     *            the adapter used to convert tested values to strings
-     * @throws NullPointerException
-     *             if either argument is null
-     */
-    public EndsWithPredicate(final String endingString, final Adapter<? super T, String> adapter) {
         if (endingString == null) {
             throw new NullPointerException("endingString is null");
         }
-        if (adapter == null) {
-            throw new NullPointerException("adapter is null");
-        }
         this.endingString = endingString;
-        this.adapter = adapter;
     }
 
     /**
@@ -70,22 +37,12 @@ public final class EndsWithPredicate<T> implements Predicate<T> {
         return this.endingString;
     }
 
-    /**
-     * Returns the adapter used to convert tested values to strings.
-     * 
-     * @return the adapter used to convert tested values to strings
-     */
-    public Adapter<? super T, String> getAdapter() {
-        return this.adapter;
-    }
-
     @Override
-    public boolean test(final T value) {
-        final String stringValue = this.getAdapter().adapt(value);
-        if (stringValue == null) {
+    public boolean test(final String value) {
+        if (value == null) {
             return false;
         }
-        return stringValue.endsWith(this.getEndingString());
+        return value.endsWith(this.getEndingString());
     }
 
     @Override
@@ -93,14 +50,11 @@ public final class EndsWithPredicate<T> implements Predicate<T> {
         if (obj == this) {
             return true;
         }
-        if (!(obj instanceof EndsWithPredicate<?>)) {
+        if (!(obj instanceof EndsWithPredicate)) {
             return false;
         }
-        final EndsWithPredicate<?> other = (EndsWithPredicate<?>) obj;
+        final EndsWithPredicate other = (EndsWithPredicate) obj;
         if (!this.getEndingString().equals(other.getEndingString())) {
-            return false;
-        }
-        if (!this.getAdapter().equals(other.getAdapter())) {
             return false;
         }
         return true;
@@ -110,13 +64,12 @@ public final class EndsWithPredicate<T> implements Predicate<T> {
     public int hashCode() {
         int result = 19;
         result = 31 * result + this.getEndingString().hashCode();
-        result = 31 * result + this.getAdapter().hashCode();
         return result;
     }
 
     @Override
     public String toString() {
-        return String.format("%s that ends with '%s'", this.getAdapter(), this.getEndingString());
+        return String.format("ends with '%s'", this.getEndingString());
     }
 
 }
--- com.bluespot.logic/src/com/bluespot/logic/predicates/StartsWithPredicate.java
+++ com.bluespot.logic/src/com/bluespot/logic/predicates/StartsWithPredicate.java
@@ -1,64 +1,31 @@
 package com.bluespot.logic.predicates;
 
-import com.bluespot.logic.Adapters;
-import com.bluespot.logic.adapters.Adapter;
-
 /**
- * Creates a predicate that evaluates the string version of some value. It will
- * return {@code true} if and only if the tested string starts with the
+ * Creates a predicate that evaluates {@link String} values. It will return
+ * {@code true} for a given string if, and only if, the string starts with the
  * predicate's specified starting string.
- * <p>
- * Values will be converted to strings using the specified adapter. By default,
- * {@link Adapters#stringValue()} is used.
  * 
  * @author Aaron Faanes
  * 
- * @param <T>
- *            the value tested in this predicate
  * @see EndsWithPredicate
  */
-public final class StartsWithPredicate<T> implements Predicate<T> {
+public final class StartsWithPredicate implements Predicate<String> {
 
     private final String startingString;
-    private final Adapter<? super T, String> adapter;
 
     /**
-     * Constructs a predicate using the specified string. Tested values are
-     * converted to string using {@link Adapters#stringValue()}. The predicate
-     * will evaluate to {@code true} if and only if the tested object has a
-     * string value that starts with the specified string value.
+     * Constructs a {@link StartsWithPredicate} using the specified string.
      * 
      * @param startingString
-     *            the value that tested strings must end with
+     *            the value that tested strings must start with
      * @throws NullPointerException
      *             if {@code startingString} is null
      */
     public StartsWithPredicate(final String startingString) {
-        this(startingString, Adapters.stringValue());
-    }
-
-    /**
-     * Constructs a predicate using the specified string. Tested values are
-     * converted to strings using the specified adapter. The predicate will
-     * evaluate to {@code true} if and only if the tested object has a string
-     * value that starts with the specified string value.
-     * 
-     * @param startingString
-     *            the value that tested strings must end with
-     * @param adapter
-     *            the adapter used to convert tested values to strings
-     * @throws NullPointerException
-     *             if either argument is null
-     */
-    public StartsWithPredicate(final String startingString, final Adapter<? super T, String> adapter) {
         if (startingString == null) {
             throw new NullPointerException("startingString is null");
         }
-        if (adapter == null) {
-            throw new NullPointerException("adapter is null");
-        }
         this.startingString = startingString;
-        this.adapter = adapter;
     }
 
     /**
@@ -70,22 +37,12 @@ public final class StartsWithPredicate<T> implements Predicate<T> {
         return this.startingString;
     }
 
-    /**
-     * Returns the adapter used to convert tested values to strings.
-     * 
-     * @return the adapter used to convert tested values to strings
-     */
-    public Adapter<? super T, String> getAdapter() {
-        return this.adapter;
-    }
-
     @Override
-    public boolean test(final T value) {
-        final String stringValue = this.getAdapter().adapt(value);
-        if (stringValue == null) {
+    public boolean test(final String value) {
+        if (value == null) {
             return false;
         }
-        return stringValue.startsWith(this.getStartingString());
+        return value.startsWith(this.getStartingString());
     }
 
     @Override
@@ -93,14 +50,11 @@ public final class StartsWithPredicate<T> implements Predicate<T> {
         if (obj == this) {
             return true;
         }
-        if (!(obj instanceof StartsWithPredicate<?>)) {
+        if (!(obj instanceof StartsWithPredicate)) {
             return false;
         }
-        final StartsWithPredicate<?> other = (StartsWithPredicate<?>) obj;
+        final StartsWithPredicate other = (StartsWithPredicate) obj;
         if (!this.getStartingString().equals(other.getStartingString())) {
-            return false;
-        }
-        if (!this.getAdapter().equals(other.getAdapter())) {
             return false;
         }
         return true;
@@ -110,13 +64,12 @@ public final class StartsWithPredicate<T> implements Predicate<T> {
     public int hashCode() {
         int result = 13;
         result = 31 * result + this.getStartingString().hashCode();
-        result = 31 * result + this.getAdapter().hashCode();
         return result;
     }
 
     @Override
     public String toString() {
-        return String.format("%s that starts with \"%s\"", this.getAdapter(), this.getStartingString());
+        return String.format("starts with \"%s\"", this.getStartingString());
     }
 
 }
