73e1b2af48357fa832936d324480824078717b56
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-07-11 13:51:59 +0000

    [javadoc] Value clarification
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@348 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

--- com.bluespot.logic/src/com/bluespot/logic/values/Value.java
+++ com.bluespot.logic/src/com/bluespot/logic/values/Value.java
@@ -1,16 +1,24 @@
 package com.bluespot.logic.values;
 
 /**
- * Value represents some value that can be retrieved at some point in time. At
- * the bare minimum, {@code Value} objects merely house some constant value.
- * While this case is useful, the purpose of {@code Value} objects is to
- * abstract the process of producing some value. In this way, they represent a
- * procedure.
+ * Represents some value that can be retrieved. {@code Value} objects abstract
+ * the process of producing some value.
  * <p>
- * Values are very different in purpose from their {@code Adapter} and {@code
- * Predicate} counterparts. They do not need to be immutable; in fact, their
- * mutability is a great asset and reason for their existence. Their main
- * intention is to wrap a process of creating or retrieving a value.
+ * The {@code Value} interface is very similar to a {@code Procedure} interface
+ * in other libraries: a single method with no arguments that returns some
+ * value. While {@code Value} objects do consist of procedures, their
+ * responsibility is to house and return some value, and I find this abstraction
+ * much more suitable than a {@code Procedure} interface.
+ * <p>
+ * {@code Value} implementations are intentionally given a wide range of freedom
+ * in how they are implemented. They do not need to be immutable nor do they
+ * need be consistent in the values they return.
+ * <p>
+ * Since this interface does not specify that exceptions are thrown, {@code
+ * Value} implementations should return {@code null} when the value cannot not
+ * be retrieved. Recovery from failed retrievals depends on the {@code Value}
+ * implementation: common examples would be IO exceptions or missing input from
+ * the user.
  * 
  * @author Aaron Faanes
  * 
