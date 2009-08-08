311507b08c497a586d1fa542c6be9b94e61800ee
dafrito <dafrito@93453b9e-65dd-4715-a0fb-a9eb6d1ddc67>
2009-08-08 01:38:56 +0000

    [new] Committed a random prototype hierarchy for a game.
    git-svn-id: http://bluespot.unfuddle.com/svn/bluespot_library/trunk@399 93453b9e-65dd-4715-a0fb-a9eb6d1ddc67

---
+++ com.bluespot.playground/src/com/bluespot/playground/game/Laser.java
@@ -0,0 +1,73 @@
+package com.bluespot.playground.game;
+
+import com.bluespot.playground.game.Team.Relation;
+
+/**
+ * A {@link Actor} that guards a position. By default, it shoots as often as
+ * possible. It will, however, deactivate if friendly actors are nearby. If both
+ * friendly and hostile targets are nearby, it will deactivate. The natural
+ * consequence is that a laser may not totally deny hostile targets access to
+ * this laser's guarded area.
+ * 
+ * @author Aaron Faanes
+ * 
+ */
+public class Laser extends AbstractActor {
+
+    private boolean shooting = true;
+
+    /**
+     * Constructs a {@link Laser} using the specified environment and team. The
+     * laser is initially being shot.
+     * 
+     * @param environment
+     *            the environment that houses this laser. It must not be null.
+     * @param team
+     *            the team that this laser is affiliated with. It must not be
+     *            null.
+     * @throws NullPointerException
+     *             if either argument is null
+     */
+    public Laser(final Environment environment, final Team team) {
+        super(environment, team);
+    }
+
+    /**
+     * Returns whether this laser is currently firing. This may be in an
+     * unreliable state if this method is used while the laser itself is
+     * iterating.
+     * 
+     * @return {@code true} if this laser is currently shooting
+     */
+    public boolean isShooting() {
+        return this.shooting;
+    }
+
+    /**
+     * Sets whether the laser is currently firing. This may be set many times
+     * during an iteration, but only the most recent change is used to actually
+     * fire or not fire the laser.
+     * 
+     * @param shooting
+     *            whether the laser is being fired
+     */
+    protected void setShooting(final boolean shooting) {
+        this.shooting = shooting;
+    }
+
+    public void update() {
+        // By default, we shoot the laser.
+        this.setShooting(true);
+        for (final Actor neighbor : this.getEnvironment().getNeighbors(this)) {
+            final Relation relation = this.getTeam().getRelations(neighbor.getTeam());
+            if (relation == Team.Relation.FRIENDLY) {
+                /*
+                 * Don't shoot friendly actors; as a consequence, hostile actors
+                 * may be allowed through.
+                 */
+                this.setShooting(false);
+                break;
+            }
+        }
+    }
+}
---
+++ com.bluespot.playground/src/com/bluespot/playground/game/Team.java
@@ -0,0 +1,54 @@
+package com.bluespot.playground.game;
+
+/**
+ * Represents a team for which {@link Actor} objects may affiliate.
+ * 
+ * @author Aaron Faanes
+ * 
+ */
+public interface Team {
+
+    /**
+     * The possible levels of animosity between two teams.
+     * 
+     * @author Aaron Faanes
+     * 
+     */
+    public static enum Relation {
+
+        /**
+         * A degenerate relationship that says a comparison was made with a
+         * logically equal team.
+         * 
+         */
+        SELF,
+
+        /**
+         * Friendly relations. Teams in this state are friendly towards one
+         * another and computer-controlled defenses will attempt to protect both
+         * teams without prejudice.
+         */
+        FRIENDLY,
+        /**
+         * Hostile relations. Teams in this state are openly aggressive towards
+         * one another.
+         */
+        HOSTILE
+    }
+
+    /**
+     * Returns the relationship between this team and the other team. Teams are
+     * required for this relationship to share the same strictness as
+     * {@link Object#equals(Object)}. Specifically, it must be transitive,
+     * symmetric, and consistent.
+     * 
+     * @param other
+     *            the other team that is tested by this method
+     * @return the relationship between this team and the specified other team.
+     *         If {@code other} is equal to this team, {@link Relation#SELF} is
+     *         returned.
+     * @throws NullPointerException
+     *             if {@code other} is null
+     */
+    public Relation getRelations(Team other);
+}
