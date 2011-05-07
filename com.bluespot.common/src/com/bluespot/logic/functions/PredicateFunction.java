44b66e6b74f933485b6cc2e9496286523a78c266
Aaron Faanes <dafrito@gmail.com>
2011-05-07 16:11:02 -0500

    Added PredicateFunction and ValueFunction

---
+++ com.bluespot.common/src/com/bluespot/logic/functions/PredicateFunction.java
@@ -0,0 +1,57 @@
+package com.bluespot.logic.functions;
+
+import com.bluespot.logic.predicates.Predicate;
+
+/**
+ * A {@link Function} that returns a value retrieved from a {@link Predicate}.
+ * 
+ * @author Aaron Faanes
+ * 
+ * @param <V>
+ *            the type of the tested value
+ */
+public class PredicateFunction<V> implements Function<V, Boolean> {
+
+	private final Predicate<? super V> predicate;
+
+	public PredicateFunction(Predicate<? super V> predicate) {
+		if (predicate == null) {
+			throw new NullPointerException("Predicate must not be null");
+		}
+		this.predicate = predicate;
+	}
+
+	public Predicate<? super V> getPredicate() {
+		return this.predicate;
+	}
+
+	@Override
+	public Boolean apply(V input) {
+		return predicate.test(input);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("PredicateFunction[%s]", predicate);
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 51;
+		result *= 31 * result + getPredicate().hashCode();
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof PredicateFunction<?>)) {
+			return false;
+		}
+		final PredicateFunction<?> other = (PredicateFunction<?>) obj;
+		return this.getPredicate().equals(other.getPredicate());
+	}
+
+}
---
+++ com.bluespot.common/src/com/bluespot/logic/functions/ValueFunction.java
@@ -0,0 +1,58 @@
+package com.bluespot.logic.functions;
+
+import com.bluespot.logic.values.Value;
+
+/**
+ * A {@link Function} that returns a value retrieved from a {@link Value}. The
+ * input is ignored, even if it is {@code null}.
+ * 
+ * @author Aaron Faanes
+ * 
+ * @param <V>
+ *            the type of the returned value
+ */
+public class ValueFunction<V> implements Function<Object, V> {
+
+	private final Value<? extends V> value;
+
+	public ValueFunction(Value<? extends V> value) {
+		if (value == null) {
+			throw new NullPointerException("Value must not be null");
+		}
+		this.value = value;
+	}
+
+	public Value<? extends V> getValue() {
+		return this.value;
+	}
+
+	@Override
+	public V apply(Object input) {
+		return value.get();
+	}
+
+	@Override
+	public String toString() {
+		return String.format("ValueFunction[%s]", value);
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 97;
+		result *= 31 * result + getValue().hashCode();
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof ValueFunction<?>)) {
+			return false;
+		}
+		final ValueFunction<?> other = (ValueFunction<?>) obj;
+		return this.getValue().equals(other.getValue());
+	}
+
+}
