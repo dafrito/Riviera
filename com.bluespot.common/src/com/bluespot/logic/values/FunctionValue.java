cb1b6e0130eec9dcec660be9dd7f059cc5f1e94d
Aaron Faanes <dafrito@gmail.com>
2011-05-07 22:35:12 -0500

    Added FunctionValue, a function evaluated at one value

---
+++ com.bluespot.common/src/com/bluespot/logic/values/FunctionValue.java
@@ -0,0 +1,61 @@
+package com.bluespot.logic.values;
+
+import com.bluespot.logic.functions.Function;
+
+public class FunctionValue<I, T> implements Value<T> {
+
+	private final Function<? super I, ? extends T> function;
+	private final I constant;
+
+	public FunctionValue(final Function<? super I, ? extends T> function, final I constant) {
+		if (function == null) {
+			throw new NullPointerException("function must not be null");
+		}
+		this.function = function;
+		this.constant = constant;
+	}
+
+	public Function<? super I, ? extends T> getFunction() {
+		return this.function;
+	}
+
+	public I getConstant() {
+		return this.constant;
+	}
+
+	@Override
+	public T get() {
+		return this.getFunction().apply(this.constant);
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof FunctionValue<?, ?>)) {
+			return false;
+		}
+		final FunctionValue<?, ?> other = (FunctionValue<?, ?>) obj;
+		if (!this.getFunction().equals(other.getFunction())) {
+			return false;
+		}
+		if (!this.getConstant().equals(other.getConstant())) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 29;
+		result = 31 * result + this.getFunction().hashCode();
+		result = 31 * result + this.getConstant().hashCode();
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("FunctionValue[function: %s, constant: %s]", this.getFunction(), this.getConstant());
+	}
+}
