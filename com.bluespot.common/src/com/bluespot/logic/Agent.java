d1ddb79adf4ebf0dc3106bfbaa5ee91752fab7ac
Aaron Faanes <dafrito@gmail.com>
2011-05-07 23:26:13 -0500

    Added a sketch of an Agent implementation

---
+++ com.bluespot.common/src/com/bluespot/logic/Agent.java
@@ -0,0 +1,74 @@
+package com.bluespot.logic;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import com.bluespot.logic.functions.Function;
+import com.bluespot.logic.functions.UnanimousFunction;
+
+/**
+ * A solver of {@link Function} objects.
+ * <p>
+ * Functions are solved in the following process:
+ * <ol>
+ * <li>Determine at least one input value accepted by the function.
+ * <li>Evaluate the provided function at those inputs.
+ * <li>For each pair, generate at least one function that, when applied the
+ * input value, generates an equal output value.
+ * </ol>
+ * <p>
+ * 
+ * @author Aaron Faanes
+ * 
+ * @param <I>
+ *            the type of input received by functions
+ * @param <V>
+ *            the returned value from functions
+ */
+public class Agent<I, V> implements Function<Function<? super I, ? extends V>, Function<? super I, ? extends V>> {
+
+	protected Iterable<? extends I> getInputs(Function<? super I, ? extends V> input) {
+		return null;
+	}
+
+	protected Collection<? extends Function<? super I, ? extends V>> getFunctions(I input, V output) {
+		return null;
+	}
+
+	@Override
+	public Function<? super I, ? extends V> apply(Function<? super I, ? extends V> function) {
+		Collection<? extends Function<? super I, ? extends V>> candidates = null;
+		for (I input : this.getInputs(function)) {
+			V output = function.apply(input);
+			Collection<? extends Function<? super I, ? extends V>> myCandidates = this.getFunctions(input, output);
+			if (candidates != null) {
+				// We run into the problem of equality here. Consider these two functions:
+				// y = x + 2
+				// y = x + 2 + 1 - 1
+				// These are logically equivalent, so at least one of them should be
+				// retained. However, they're not equal according to Object#equals, so
+				// we'd lose this result. 
+				// 
+				// To resolve this, we either need some simplifying process, or we need to
+				// ensure that functions are always produced in their simplest form.
+				//
+				// Also consider the possibility that the result might be composed of two
+				// separate functions. In this case, there may not be a single function that
+				// fits both results. In this scenario, we should attempt to create a
+				// piece-wise function from the discarded candidates.
+				candidates.retainAll(myCandidates);
+			} else {
+				candidates = new HashSet<Function<? super I, ? extends V>>(myCandidates);
+			}
+			if (candidates.isEmpty()) {
+				return null;
+			} else if (candidates.size() == 1) {
+				return candidates.iterator().next();
+			}
+		}
+		if (candidates == null || candidates.isEmpty()) {
+			return null;
+		}
+		return new UnanimousFunction<I, V>(candidates);
+	}
+}
