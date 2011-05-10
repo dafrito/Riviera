64c2aaa26449d561e13ae480faa70db1926a8772
Aaron Faanes <dafrito@gmail.com>
2011-05-09 21:27:27 -0500

    First implementation of SafeCurryable
    This class is a beast.

--- com.bluespot.common/src/com/bluespot/logic/functions/Functions.java
+++ com.bluespot.common/src/com/bluespot/logic/functions/Functions.java
@@ -2,6 +2,7 @@ package com.bluespot.logic.functions;
 
 import com.bluespot.logic.adapters.Adapter;
 import com.bluespot.logic.adapters.Adapters;
+import com.bluespot.logic.adapters.SafeFunctionAdapter;
 import com.bluespot.logic.predicates.Predicate;
 import com.bluespot.logic.values.FunctionValue;
 import com.bluespot.logic.values.Value;
@@ -102,6 +103,140 @@ public class Functions {
 
 	public static <C, R extends Function<?, ?>> CurryFunction<C, R> curry(C value) {
 		return new CurryFunction<C, R>(value);
+	}
+
+	/**
+	 * Protect the specified {@link Curryable} through adapters. The curried
+	 * value is adapted to the expected type {@code C} for the underlying
+	 * curryable. The returned function is similarly protected by adapted
+	 * received values to the function's input type {@code I}.
+	 * 
+	 * @param <C>
+	 *            the type of value that is curried
+	 * @param <I>
+	 *            the type of input value received by underlying functions.
+	 * @param <R>
+	 *            the type of value that is returned by underlying functions.
+	 *            The return type is not modified by this object.
+	 * @param curryAdapter
+	 *            an {@link Adapter} that converts any object to a type accepted
+	 *            by the underlying curryable
+	 * @param curryable
+	 *            the {@link Curryable} that is protected by this invocation
+	 * @param functionAdapter
+	 *            an {@link Adapter} that converts functions returned by the
+	 *            underlying {@link Curryable} to {@link SafeFunction} objects
+	 * @return a {@link SafeCurryable} that accepts any value, and produces
+	 *         {@link SafeFunction} objects that also accept any value
+	 * @see AdaptingSafeCurryable
+	 * @see SafeFunction
+	 */
+	public static <C, I, R> SafeCurryable<SafeFunction<? extends R>> protect(
+			Adapter<? super Object, ? extends C> curryAdapter,
+			Curryable<? super C, ? extends Function<? super I, ? extends R>> curryable,
+			Adapter<? super Function<? super I, ? extends R>, ? extends SafeFunction<? extends R>> functionAdapter) {
+		return new AdaptingSafeCurryable<C, I, R>(curryAdapter, curryable, functionAdapter);
+	}
+
+	/**
+	 * Protect the specified {@link Curryable} through adapters.
+	 * <p>
+	 * Curried values are adapted using {@link Adapters#cast(Class)}. Any value
+	 * that is not an instance of {@code curryType} is converted to {@code null}.
+	 * 
+	 * @param <C>
+	 *            the type of value that is curried
+	 * @param <I>
+	 *            the type of input value received by underlying functions.
+	 * @param <R>
+	 *            the type of value that is returned by underlying functions.
+	 *            The return type is not modified by this object.
+	 * @param curryType
+	 *            the type that is used to dynamically cast objects to a class
+	 *            expected by the underlying {@link Curryable}.
+	 * @param curryable
+	 *            the {@link Curryable} that is protected by this invocation
+	 * @param functionAdapter
+	 *            an {@link Adapter} that converts functions returned by the
+	 *            underlying {@link Curryable} to {@link SafeFunction} objects
+	 * @return a {@link SafeCurryable} that dynamically casts curry values, and
+	 *         produces {@link SafeFunction} objects that also accept any value
+	 * @see Functions#protect(Adapter, Curryable, Adapter)
+	 */
+	public static <C, I, R> SafeCurryable<SafeFunction<? extends R>> protect(
+			Class<? extends C> curryType,
+			Curryable<? super C, ? extends Function<? super I, ? extends R>> curryable,
+			Adapter<? super Function<? super I, ? extends R>, ? extends SafeFunction<? extends R>> functionAdapter) {
+		return Functions.protect(Adapters.cast(curryType), curryable, functionAdapter);
+	}
+
+	/**
+	 * Protect the specified {@link Curryable} through adapters.
+	 * <p>
+	 * The returned function is protected via {@link Adapters#cast(Class)}, such
+	 * that any value that is not a subclass of {@code functionInputType} will
+	 * be converted to null.
+	 * 
+	 * @param <C>
+	 *            the type of value that is curried
+	 * @param <I>
+	 *            the type of input value received by underlying functions.
+	 * @param <R>
+	 *            the type of value that is returned by underlying functions.
+	 *            The return type is not modified by this object.
+	 * @param curryAdapter
+	 *            an {@link Adapter} that converts any object to a type accepted
+	 *            by the underlying curryable
+	 * @param curryable
+	 *            the {@link Curryable} that is protected by this invocation
+	 * @param functionInputType
+	 *            the type that is used to cast objects to a type expected by
+	 *            functions produced by the underlying curryable.
+	 * @return a {@link SafeCurryable} that accepts any value, and produces
+	 *         {@link SafeFunction} objects that also accept any value
+	 * @see Functions#protect(Adapter, Curryable, Adapter)
+	 */
+	public static <C, I, R> SafeCurryable<SafeFunction<? extends R>> protect(
+			Adapter<? super Object, ? extends C> curryAdapter,
+			Curryable<? super C, ? extends Function<? super I, ? extends R>> curryable,
+			Class<? extends I> functionInputType) {
+		return Functions.protect(curryAdapter, curryable, new SafeFunctionAdapter<I, R>(Adapters.cast(functionInputType)));
+	}
+
+	/**
+	 * Protect the specified {@link Curryable} through adapters.
+	 * <p>
+	 * Curried values are adapted using {@link Adapters#cast(Class)}. Any value
+	 * that is not an instance of {@code curryType} is converted to {@code null}.
+	 * <p>
+	 * The returned function is protected via {@link Adapters#cast(Class)}, such
+	 * that any value that is not a subclass of {@code functionInputType} will
+	 * be converted to null.
+	 * 
+	 * @param <C>
+	 *            the type of value that is curried
+	 * @param <I>
+	 *            the type of input value received by underlying functions.
+	 * @param <R>
+	 *            the type of value that is returned by underlying functions.
+	 *            The return type is not modified by this object.
+	 * @param curryType
+	 *            the type that is used to dynamically cast objects to a class
+	 *            expected by the underlying {@link Curryable}.
+	 * @param curryable
+	 *            the {@link Curryable} that is protected by this invocation
+	 * @param functionInputType
+	 *            the type that is used to cast objects to a type expected by
+	 *            functions produced by the underlying curryable.
+	 * @return a {@link SafeCurryable} that accepts any value, and produces
+	 *         {@link SafeFunction} objects that also accept any value
+	 * @see Functions#protect(Adapter, Curryable, Adapter)
+	 */
+	public static <C, I, R> SafeCurryable<SafeFunction<? extends R>> protect(
+			Class<? extends C> curryType,
+			Curryable<? super C, ? extends Function<? super I, ? extends R>> curryable,
+			Class<? extends I> functionInputType) {
+		return Functions.protect(Adapters.cast(curryType), curryable, new SafeFunctionAdapter<I, R>(Adapters.cast(functionInputType)));
 	}
 
 	public static <I, V> Function<? super I, ? extends V> guard(Predicate<? super I> guard, Function<? super I, ? extends V> function) {
