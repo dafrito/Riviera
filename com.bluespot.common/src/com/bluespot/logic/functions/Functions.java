a90627b5eb40173c0dd201973934f65f824516d1
Aaron Faanes <dafrito@gmail.com>
2011-05-09 23:49:10 -0500

    Added MetaCurryable

--- com.bluespot.common/src/com/bluespot/logic/functions/Functions.java
+++ com.bluespot.common/src/com/bluespot/logic/functions/Functions.java
@@ -37,37 +37,18 @@ public class Functions {
 	}
 
 	/**
-	 * Return {@link CurryFunction} objects that use provided values.
-	 * <p>
-	 * This singleton would be an anonymous class, but I couldn't get the
-	 * casting to work with the generic type. A singleton was the next best
-	 * thing.
+	 * Get an instance of {@link MetaCurryable}. This object behaves as a
+	 * function as well as a curryable, and produces functions that curry
+	 * values.
 	 * 
-	 * @author Aaron Faanes
-	 * 
-	 * @param <I>
-	 *            the type of the provided value
+	 * @param <C>
+	 *            the type of values that will be curried
+	 * @param <F>
+	 *            the type of functions produced by curried functions
+	 * @return an instance of {@link MetaCurryable}
 	 */
-	private static class CreateCurryFunction<I> implements Function<I, CurryFunction<I, Function<?, ?>>> {
-
-		private CreateCurryFunction() {
-			// Hide this constructor, since we're a singleton
-		}
-
-		@Override
-		public CurryFunction<I, Function<?, ?>> apply(I input) {
-			return new CurryFunction<I, Function<?, ?>>(input);
-		}
-
-		public static final CreateCurryFunction<?> INSTANCE = new CreateCurryFunction<Object>();
-	}
-
-	@SuppressWarnings("unchecked")
-	public static <I> Function<I, CurryFunction<I, Function<?, ?>>> curry() {
-		// This unchecked cast is always safe, since we're only restricting the allowable values here.
-		// Uses of this function would be restricted by the returned type anyway, and thus fail at
-		// compile-time.
-		return (CreateCurryFunction<I>) CreateCurryFunction.INSTANCE;
+	public static <C, F extends Function<?, ?>> MetaCurryable<C, F> curry() {
+		return MetaCurryable.newInstance();
 	}
 
 	/**
---
+++ com.bluespot.common/src/com/bluespot/logic/functions/MetaCurryable.java
@@ -0,0 +1,58 @@
+package com.bluespot.logic.functions;
+
+/**
+ * A {@link Curryable} that curries values.
+ * <p>
+ * It some ways, this class represents a fundamental unit of dynamic
+ * programming, as it produces functions that produce curryables. It implements
+ * both {@link Curryable} and {@link Function} to provide users with either
+ * method.
+ * 
+ * @author Aaron Faanes
+ * 
+ * @param <C>
+ *            the type of value that will be curried
+ * @param <F>
+ *            the type of function produced by curried objects
+ * @see #newInstance()
+ */
+public final class MetaCurryable<C, F extends Function<?, ?>> implements Curryable<C,
+		Function<Curryable<? super C, ? extends F>, ? extends F>>,
+		Function<C, Function<Curryable<? super C, ? extends F>, ? extends F>> {
+
+	private MetaCurryable() {
+		// Hide this constructor since we're a singleton
+	}
+
+	@Override
+	public Function<Curryable<? super C, ? extends F>, ? extends F> curry(C value) {
+		return new CurryFunction<C, F>(value);
+	}
+
+	@Override
+	public Function<Curryable<? super C, ? extends F>, ? extends F> apply(C input) {
+		return new CurryFunction<C, F>(input);
+	}
+
+	private static final MetaCurryable<?, ?> INSTANCE = new MetaCurryable<Object, Function<?, ?>>();
+
+	/**
+	 * Get an instance of {@link MetaCurryable}. This method is allowed to
+	 * return the same instance, as {@code MetaCurryable} objects may not have
+	 * any reified state; a single instance cast to the correct generic types
+	 * will suffice.
+	 * 
+	 * @param <C>
+	 *            the type of value that will be curried
+	 * @param <F>
+	 *            the type of function produced by curried objects
+	 * @return a {@link MetaCurryable} instance
+	 * @see MetaCurryable
+	 */
+	@SuppressWarnings("unchecked")
+	public static <C, F extends Function<?, ?>> MetaCurryable<C, F> newInstance() {
+		// This unsafe cast will always succeed, since MetaCurryable has no state.
+		return (MetaCurryable<C, F>) INSTANCE;
+	}
+
+}
