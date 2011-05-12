ffce890d101908bd989ac5fe48480126da6c10fd
Aaron Faanes <dafrito@gmail.com>
2011-05-11 20:32:30 -0500

    Made only the compose-function tests use safeComposing

--- com.bluespot.common/test/com/bluespot/logic/AgentTests.java
+++ com.bluespot.common/test/com/bluespot/logic/AgentTests.java
@@ -1,7 +1,7 @@
 package com.bluespot.logic;
 
-import java.util.Collection;
 import java.util.HashSet;
+import java.util.Set;
 
 import junit.framework.Assert;
 
@@ -23,8 +23,8 @@ public class AgentTests {
 	 * 
 	 * @return a pool of values usable by {@link InputGenerator} objects
 	 */
-	private Collection<Object> getPool() {
-		Collection<Object> values = new HashSet<Object>();
+	private Set<Object> getPool() {
+		Set<Object> values = new HashSet<Object>();
 		values.add(0.0d);
 		values.add(1.0d);
 		for (NumericOperation op : NumericOperations.values()) {
@@ -70,7 +70,9 @@ public class AgentTests {
 
 	@Test
 	public void testAgentDeducesComposedFunctions() throws Exception {
-		Agent<Double, Number> agent = new Agent<Double, Number>(Double.class, getPool());
+		Set<Object> pool = getPool();
+		pool.add(Functions.safeComposing());
+		Agent<Double, Number> agent = new Agent<Double, Number>(Double.class, pool);
 
 		Assert.assertNotNull(agent.apply(Functions.compose(Functions.add(3), Functions.multiply(2))));
 		Assert.assertNotNull(agent.apply(new Function<Double, Number>() {
