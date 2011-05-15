8dd3c2e3ca9e113a8722c4365c890f1913e9bff6
Aaron Faanes <dafrito@gmail.com>
2011-05-14 19:48:03 -0500

    AbstractPoint now implements a bit more functionality

--- com.bluespot.common/src/com/bluespot/geom/points/AbstractPoint3.java
+++ com.bluespot.common/src/com/bluespot/geom/points/AbstractPoint3.java
@@ -3,6 +3,8 @@
  */
 package com.bluespot.geom.points;
 
+import com.bluespot.geom.Axis;
+
 /**
  * A basic {@link Point3} implementation.
  * 
@@ -22,6 +24,53 @@ public abstract class AbstractPoint3<P extends Point3<P>> implements Point3<P> {
 	@Override
 	public boolean isMutable() {
 		return this.mutable;
+	}
+
+	@Override
+	public P with(Axis axis, P point) {
+		if (axis == null) {
+			throw new NullPointerException("Axis must not be null");
+		}
+		if (point == null) {
+			throw new NullPointerException("Point must not be null");
+		}
+		P result = this.toMutable();
+		result.set(axis, point);
+		return result;
+	}
+
+	@Override
+	public P added(P point) {
+		P result = this.toMutable();
+		result.add(point);
+		return result;
+	}
+
+	@Override
+	public P added(Axis axis, P point) {
+		if (axis == null) {
+			throw new NullPointerException("Axis must not be null");
+		}
+		if (point == null) {
+			throw new NullPointerException("Point must not be null");
+		}
+		P result = this.toMutable();
+		result.add(axis, point);
+		return result;
+	}
+
+	@Override
+	public P cleared(Axis axis) {
+		P result = this.toMutable();
+		result.clear(axis);
+		return result;
+	}
+
+	@Override
+	public P interpolated(P dest, float offset) {
+		P result = this.toMutable();
+		result.interpolate(dest, offset);
+		return result;
 	}
 
 }
