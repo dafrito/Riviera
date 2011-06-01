0e4a7df086d5af009eb3989ba6957ec2de72b139
Aaron Faanes <dafrito@gmail.com>
2011-06-01 02:28:27 -0500

    Added Offset4i and supporting classes

---
+++ com.bluespot.common/src/com/bluespot/geom/Side.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.bluespot.geom;
+
+/**
+ * @author Aaron Faanes
+ * 
+ */
+public enum Side {
+	LEFT, RIGHT, TOP, BOTTOM, VERTICAL, HORIZONTAL;
+}
---
+++ com.bluespot.common/src/com/bluespot/geom/offsets/AbstractOffset4.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.bluespot.geom.offsets;
+
+import com.bluespot.geom.Side;
+
+
+/**
+ * An {@link Offset4i} that implements some basic functionality. Most
+ * implemented functions deal with copy-and-mutate methods, since these don't
+ * depend on internals.
+ * 
+ * @author Aaron Faanes
+ * @param <T>
+ *            the type of offset
+ * 
+ */
+public abstract class AbstractOffset4<T extends Offset4<T>> implements Offset4<T> {
+
+	private final boolean mutable;
+
+	protected AbstractOffset4(boolean mutable) {
+		this.mutable = mutable;
+	}
+
+	@Override
+	public boolean isMutable() {
+		return this.mutable;
+	}
+
+	@Override
+	public T with(Side side, T offset) {
+		if (side == null) {
+			throw new NullPointerException("side must not be null");
+		}
+		if (offset == null) {
+			throw new NullPointerException("offset must not be null");
+		}
+		T result = this.toMutable();
+		result.set(side, offset);
+		return result;
+	}
+
+	@Override
+	public T added(T offset) {
+		T result = this.toMutable();
+		result.add(offset);
+		return result;
+	}
+
+	@Override
+	public T added(Side side, T offset) {
+		if (side == null) {
+			throw new NullPointerException("side must not be null");
+		}
+		if (offset == null) {
+			throw new NullPointerException("offset must not be null");
+		}
+		T result = this.toMutable();
+		result.add(side, offset);
+		return result;
+	}
+
+	@Override
+	public T cleared(Side side) {
+		T result = this.toMutable();
+		result.clear(side);
+		return result;
+	}
+
+}
