57567b50f69d7d7b9ca66c23a65b4f103f9269a0
Aaron Faanes <dafrito@gmail.com>
2011-06-11 03:24:05 -0500

    Added a couple Iterator tests

--- com.bluespot.common/test/com/bluespot/logic/IteratorTests.java
+++ com.bluespot.common/test/com/bluespot/logic/IteratorTests.java
@@ -3,7 +3,10 @@
  */
 package com.bluespot.logic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 import junit.framework.Assert;
 
@@ -33,5 +36,28 @@ public class IteratorTests {
 				Arrays.asList(0, 1, 1, 2, 3, 5, 8, 13, 21, 34),
 				Iterators.fill(Iterators.fibonacci(), 10)
 				);
+	}
+
+	@Test
+	public void staggeredIteratorOperatesSuccessivelyOnUnderlyingIterators() throws Exception {
+		Assert.assertEquals(
+				Arrays.asList(0, 2, 3, 1, 1, 3, 2, 0, 3),
+				Iterators.fill(
+						Iterators.stagger(Iterators.range(0, 2)).add(
+								Iterators.range(2, 0)).add(
+								Iterators.constant(3)),
+								9));
+	}
+
+	@Test
+	public void iteratorsHasNextIsIdempotent() throws Exception {
+		Iterator<Integer> iter = Iterators.chain(Iterators.count(0, 1, 3)).add(Iterators.count(3, -1, 0));
+		List<Integer> nums = new ArrayList<Integer>();
+		while (iter.hasNext()) {
+			// Spurious call!
+			iter.hasNext();
+			nums.add(iter.next());
+		}
+		Assert.assertEquals(Arrays.asList(0, 1, 2, 3, 3, 2, 1, 0), nums);
 	}
 }
