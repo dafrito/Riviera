dbf45f19ff3832390d5d9ed9bf9161fba90c35cd
Aaron Faanes <dafrito@gmail.com>
2011-06-12 02:53:48 -0500

    Added some theoretical token interfaces

---
+++ com.bluespot.common/src/com/bluespot/parsing/Token.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.bluespot.parsing;
+
+/**
+ * A parsed token of data.
+ * 
+ * @author Aaron Faanes
+ * @param <V>
+ *            the type of {@link TokenVisitor} that can visit tokens of this
+ *            type.
+ * @see Parser
+ * @see TokenVisitor
+ * @see TokenFactory
+ */
+public interface Token<V extends TokenVisitor> {
+	public void accept(V visitor);
+}
---
+++ com.bluespot.common/src/com/bluespot/parsing/TokenFactory.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.bluespot.parsing;
+
+/**
+ * A factory that creates {@link Token} objects.
+ * 
+ * @author Aaron Faanes
+ * @param <T>
+ *            the type of {@link Token} created by this factory
+ * 
+ */
+public interface TokenFactory<T extends Token<?>> {
+	public T create(String content);
+}
