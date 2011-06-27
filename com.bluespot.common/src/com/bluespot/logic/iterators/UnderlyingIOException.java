d46502643595f3cb54f718d3a25d1294d4a6a7c5
Aaron Faanes <dafrito@gmail.com>
2011-06-27 06:26:02 -0500

    UnderlyingIOException now has an underlying IOException

--- com.bluespot.common/src/com/bluespot/logic/iterators/UnderlyingIOException.java
+++ com.bluespot.common/src/com/bluespot/logic/iterators/UnderlyingIOException.java
@@ -8,7 +8,18 @@ import java.io.IOException;
 public class UnderlyingIOException extends IllegalStateException {
 	private static final long serialVersionUID = -1512303122764302644L;
 
+	private final IOException underlying;
+
 	public UnderlyingIOException(IOException ex) {
 		super(ex);
+		if (ex == null) {
+			throw new NullPointerException("ex must not be null");
+		}
+		this.underlying = ex;
+	}
+
+	@Override
+	public IOException getCause() {
+		return this.underlying;
 	}
 }
