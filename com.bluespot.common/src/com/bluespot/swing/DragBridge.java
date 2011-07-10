dec67f9e0958ac0957f9239667d930eb35b4bc79
Aaron Faanes <dafrito@gmail.com>
2011-07-10 04:44:41 -0500

    Added DragBridge, a producer of vectors from drag events

---
+++ com.bluespot.common/src/com/bluespot/swing/DragBridge.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package com.bluespot.swing;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseMotionListener;
+
+import javax.swing.JComponent;
+
+import com.bluespot.geom.vectors.Vector3i;
+import com.bluespot.logic.actors.Actor;
+import com.bluespot.logic.actors.Producer;
+
+/**
+ * @author Aaron Faanes
+ * 
+ */
+public class DragBridge extends Producer<Vector3i> {
+
+	private final JComponent component;
+
+	private final Vector3i last = Vector3i.mutable();
+
+	public DragBridge(final JComponent component, final Actor<? super Vector3i> receiver) {
+		super(receiver);
+		if (component == null) {
+			throw new NullPointerException("component must not be null");
+		}
+		this.component = component;
+		component.addMouseListener(mouseListener);
+
+	}
+
+	private final MouseListener mouseListener = new MouseAdapter() {
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			component.addMouseMotionListener(motionListener);
+			last.set(e.getX(), e.getY());
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			component.removeMouseMotionListener(motionListener);
+		}
+	};
+
+	private final MouseMotionListener motionListener = new MouseMotionAdapter() {
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			produce(Vector3i.frozen(e.getX() - last.x(), e.getY() - last.y()));
+			last.set(e.getX(), e.getY());
+		}
+
+	};
+
+	public void detach() {
+		component.removeMouseListener(this.mouseListener);
+		component.removeMouseMotionListener(this.motionListener);
+	}
+}
