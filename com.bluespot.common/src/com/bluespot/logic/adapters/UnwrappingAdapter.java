f4183c4005018b7c8ad7e89cfb1703d59889c36a
Aaron Faanes <dafrito@gmail.com>
2011-07-10 05:08:05 -0500

    Added UnwrappingAdapter, which unwraps Value<T> to T's

---
+++ com.bluespot.common/src/com/bluespot/logic/adapters/UnwrappingAdapter.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.bluespot.logic.adapters;
+
+import com.bluespot.logic.values.Value;
+
+/**
+ * Retrieves a value from a {@link Value} implementation. Null values are
+ * returned directly.
+ * 
+ * @author Aaron Faanes
+ * @param <T>
+ *            the type of wrapped value
+ * @see Value
+ */
+public class UnwrappingAdapter<T> implements Adapter<Value<? extends T>, T> {
+
+	@Override
+	public T adapt(Value<? extends T> source) {
+		if (source == null) {
+			return null;
+		}
+		return source.get();
+	}
+
+}
