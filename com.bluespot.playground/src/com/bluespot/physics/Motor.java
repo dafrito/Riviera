159371faa5c4a045ed55f38c959c40e7bc35dd8c
Aaron Faanes <dafrito@gmail.com>
2013-05-27 03:18:48 -0500

    Some changes to how motors work (and added tests)

---
+++ com.bluespot.playground/src/com/bluespot/physics/Motor.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2011 Aaron Faanes
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.bluespot.physics;
+
+import com.bluespot.logic.actors.Actor;
+import com.bluespot.logic.actors.LongActor;
+import com.bluespot.timing.Clock;
+
+/**
+ * @author Aaron Faanes
+ * 
+ */
+public class Motor {
+
+	private final double maximum;
+	private double power;
+	private final Clock clock;
+
+	public Motor(final Clock clock) {
+		this(clock, Double.POSITIVE_INFINITY);
+	}
+
+	public Motor(final Clock clock, final double maxPower) {
+		if (clock == null) {
+			throw new NullPointerException("clock must not be null");
+		}
+		this.clock = clock;
+		if (Double.isNaN(maxPower)) {
+			throw new IllegalArgumentException("maximum output must not be NaN");
+		}
+		this.maximum = maxPower;
+	}
+
+	public double getMaximumPower() {
+		return maximum;
+	}
+
+	public Clock getClock() {
+		return clock;
+	}
+
+	public double getPower() {
+		return power;
+	}
+
+	public void setPower(final double power) {
+		if (Double.isNaN(power)) {
+			throw new IllegalArgumentException("output must not be NaN");
+		}
+		this.power = Math.max(0, Math.min(power, maximum));
+	}
+
+	public void connect(final Actor<Double> target) {
+		if (target == null) {
+			throw new NullPointerException("target must not be null");
+		}
+		clock.listen(new LongActor() {
+			@Override
+			public void receive(long value) {
+				target.receive(power * value);
+			}
+		});
+	}
+}
---
+++ com.bluespot.playground/test/com/bluespot/physics/MotorTests.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2011 Aaron Faanes
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.bluespot.physics;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.bluespot.logic.actors.Actor;
+import com.bluespot.timing.MutableClock;
+
+/**
+ * @author Aaron Faanes
+ * 
+ */
+public class MotorTests {
+
+	private Motor newMotor() {
+		return new Motor(new MutableClock());
+	}
+
+	private Motor newMotor(double power) {
+		return new Motor(new MutableClock(), power);
+	}
+
+	/**
+	 * Presumably, a physics system would provide motors to ensure the maximum
+	 * output is reasonable, but motors can also be created in a headless
+	 * fashion. Of course, there should be some constraint that disallows
+	 * introducing these headless motors into a preexisting physics system.
+	 */
+	@Test
+	public void motorsCanBeHeadless() {
+		newMotor();
+	}
+
+	/**
+	 * This seems like the natural thing to do, but I don't really like having
+	 * positive infinity introduced so carelessly.
+	 */
+	@Test
+	public void motorsDefaultToInfiniteMaximumOutput() {
+		Motor motor = new Motor(new MutableClock());
+		assertEquals(Double.POSITIVE_INFINITY, motor.getMaximumPower(), 0d);
+	}
+
+	@Test
+	public void motorsRefuseToExceedTheirMaximumOutput() {
+		Motor motor = newMotor(40);
+		assertEquals(40d, motor.getMaximumPower(), 1e-3);
+		motor.setPower(80);
+		assertEquals(40d, motor.getPower(), 1e-3);
+	}
+
+	@Test
+	public void motorsCanBeTurnedOff() {
+		Motor motor = newMotor(40);
+		motor.setPower(80);
+		motor.setPower(0);
+		assertEquals(0d, motor.getPower(), 1e-3);
+	}
+
+	@Test
+	public void motorOutputCannotBeNegative() {
+		Motor motor = newMotor(40);
+		motor.setPower(40);
+		motor.setPower(-10);
+		assertEquals(0d, motor.getPower(), 1e-3);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void motorsCrashOnNaN() {
+		newMotor(Double.NaN);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void motorOutputCannotBeNaN() {
+		Motor motor = newMotor(10);
+		motor.setPower(Double.NaN);
+	}
+
+	@Test
+	public void motorsCanOnlyPerformWorkAfterTimeDuration() throws Exception {
+		final MutableClock clock = new MutableClock();
+		final Motor motor = new Motor(clock, 40);
+		motor.connect(new Actor<Double>() {
+			@Override
+			public void receive(Double value) {
+				Assert.assertEquals(motor.getPower() * clock.getElapsed(), value);
+			}
+		});
+		clock.elapse(10);
+	}
+}
